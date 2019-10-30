6acaede94d21777736ebb25da91840c949c1771f
xiaoluoboding <xiaoluoboding@gmail.com>
2019-10-30 20:33:00 +0800

    refactor: 按需引入第三方扩展组件

--- src/main.js
+++ src/main.js
@@ -4,30 +4,31 @@ import 'normalize.css'
 import './plugins/json-viewer'
 import './plugins/clipboard2'
 
-import App from './App.vue'
 import router from './router'
 import store from './store'
 
 import ElementUI from 'element-ui'
 import '../theme/index.css'
+
 // 引入codeMirror的样式
+import 'codemirror/lib/codemirror.css'
 import 'codemirror/theme/cobalt.css'
 import 'codemirror/mode/javascript/javascript.js'
 
-import SchemaForm, {
-  SchemaFormItem,
-  SchemaFormJsoneditor,
-  SchemaFormQuill,
-  SchemaFormCodemirror
-} from '@vueblocks/element-schema-form'
+import SchemaForm, { SchemaFormItem } from '@vueblocks/element-schema-form'
+
+import SchemaFormQuill from '@vueblocks/element-schema-form/lib/SchemaFormQuill.common.min'
+import SchemaFormCodemirror from '@vueblocks/element-schema-form/lib/SchemaFormCodemirror.common.min'
+
+import App from './App.vue'
 
 Vue.use(ElementUI, {
   size: 'small'
 })
-Vue.component('SchemaFormJsoneditor', SchemaFormJsoneditor)
-Vue.component('SchemaFormQuill', SchemaFormQuill)
 
+Vue.component('SchemaFormQuill', SchemaFormQuill)
 Vue.component('SchemaFormCodemirror', SchemaFormCodemirror)
+
 Vue.component('SchemaFormItem', SchemaFormItem)
 
 Vue.use(SchemaForm, {
